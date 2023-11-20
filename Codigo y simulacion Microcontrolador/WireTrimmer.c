028e670abc28133753bc5291765c09e09b5fc07e
Joel_Zufff <diegodjzf@gmail.com>
2023-11-20 02:46:23 -0600

    Agregue estructura de ordenes de impresion, cambie estructura de menu por funcion de seleccion de numeros

--- Codigo y simulacion Microcontrolador/WireTrimmer.c
+++ Codigo y simulacion Microcontrolador/WireTrimmer.c
@@ -12,49 +12,51 @@
 #define     LeftButton     PIN_C0
 #define     RightButton    PIN_C1
 #define     OKButton       PIN_C2
-
-//   Variables Constantes  //
 #define     Left     1
 #define     Right    0
+
+//   Variables Constantes  //
 #define     DelayMP  500
 
 // Estructuras //
-struct menu
+struct step_motor
 {
-   char           *MenuOptions[16];          // Arreglo de cadenas de caracteres
-   int            OptionMaxIndex;            // Maximo indice para MenuOptions
-   signed int     ActualOption;              // Indice actual para MenuOptions
+   long           Steps[4];                  // Pines de cada paso del motor
+   signed int     PosIndex;                  // Posicion actual del motor
 };
 
-struct stepmotor
+struct wire_print_order
 {
-   long          Steps[4];                   // Pines de cada paso del motor
-   signed int     PosIndex;                  // Posicion actual del motor
+   int16          Length;                    // Longitud del cable (mm)
+   int            PeelingLength;             // Longitud de pelado de cable (mm)
+   int            Gauge;                     // Calibre
+   int            Amount;                    // Cantidad
 };
 
 #int_rda
 void ComputerConection() // Activar y gestionar conexion con interfaz de computadora
 {
-   // Deteccion de coneccion con computadora
+   
 }
 
 //  Prototipos de funcion  //
-void MenuSelect(struct menu*);
-void MotorSteps(int16 Amount, struct stepmotor*, int1 turnDirection); // Se mueve un motor "Amount" veces
-void MotorSteps(int16 Amount, struct stepmotor*, struct stepmotor*, int1 turnDirection); // Sobrecarga para mover 2 motores a la vez
+int16 NumberSelect(int16 MaxNumber, signed int Exponent);               // Funcion para seleccion de numero
+void MotorSteps(int16 Amount, struct step_motor*, int1 turnDirection);  // Se mueve un motor "Amount" veces
+void MotorSteps(int16 Amount, struct step_motor*, struct step_motor*, int1 turnDirection); // Sobrecarga para mover 2 motores a la vez
 
 void main()
 {
    // Activamos Interrupciones
    enable_interrupts(GLOBAL);       enable_interrupts(int_rda);
-   
-   // Defino los menus
-   struct menu Principal = {{"Realizar Corte", "Configuraciones"}, 1, 0};
-   
-   // Defino los motores a pasos
-   struct stepmotor WireMotor1 = {{PIN_B0, PIN_B1, PIN_B2, PIN_B3}, 0};
-   struct stepmotor WireMotor2 = {{PIN_B4, PIN_B5, PIN_B6, PIN_B7}, 0};
-   
+      
+   // Declaro e inicializo los motores a pasos
+   struct step_motor WireMotor1 = {{PIN_B0, PIN_B1, PIN_B2, PIN_B3}, 0};
+   struct step_motor WireMotor2 = {{PIN_B4, PIN_B5, PIN_B6, PIN_B7}, 0};
+
+   // Obtenemos longitud del cartucho de cable de la memoria eeprom (cm)
+   //float Reel = read_eeprom(1);
+   float WireReel = 999.99;
+
    // Mensaje Introductorio
    lcd_init();       lcd_gotoxy(3,1);
    printf(lcd_putc,"WIRE TRIMMER\n Prog. Avanzada");
@@ -62,59 +64,66 @@ void main()
    
    while(true)
    {
-      // Revisar disponibilidad de cable con memoria eeprom, y con los foto-transistores.
-      // Si los 2 estan bien comenzar impresion de menu principal
-
+      // Revisar disponibilidad de cable con memoria eeprom, y con sensor de presencia de cable.
       // Revision de booleano para control con computadora
-      
-      MenuSelect(&Principal);
 
-      switch (Principal.ActualOption)
-      {
-         case 0: // Realizar Corte
-         {
-            
-         }
-         break;
-         case 1: // Configuraciones
-         {
-            
-         }
-         break;
-         default: break;
-      }
+      // Falta validacion de datos para ver forma de restringir pedir mas cable del disponible
+      // Ocupo calcular Exponente dentro de la funcion a partir del numero maximo y mandarle que el numero maximo sea el cartucho disponible
+      
+      struct wire_print_order ActualOrder = {0,0,0,0};
+      
+      printf(lcd_putc, "\fLongitud (mm):");
+      ActualOrder.Length = NumberSelect(1000,3);
+
+      printf(lcd_putc, "\fPelado (mm):");
+      ActualOrder.Length = NumberSelect(10,1);
    }
 }
 
-void MenuSelect(struct menu* OpenMenu) // Funcion para impresion y seleccion de menus
+int16 NumberSelect(int16 MaxNumber, signed int Exponent)
 {
-   int1 BoolLCD = 1;    OpenMenu->ActualOption = 0; // Reiniciamos opciones
-   
-   while(!input(OKButton)) // Mientras el usuario no escoja una opcion
+   int1 PrintBool = 0;   int16 Number = 0;    int auxExp = Exponent;
+
+   printf(lcd_putc, "\n------0000------");
+   lcd_gotoxy(10-Exponent,2);   lcd_putc('*');
+
+   while(Exponent >= 0)
    {
-      if (input(LeftButton))
+      int16 aux = 1;
+
+      if(PrintBool)
       {
-        while(input(LeftButton));
-        OpenMenu->ActualOption--;    BoolLCD = 1;
+         lcd_gotoxy(7,2);
+         printf(lcd_putc,"%04lu", Number);         
+         PrintBool = 0;
+      }
+      
+      if(input(OKButton))
+      {
+         while (input(OKButton));   Exponent--;
+         lcd_gotoxy(7,2);           printf(lcd_putc,"%04lu", Number);
+         lcd_gotoxy(10-Exponent,2);   lcd_putc('*');
+      }
+      else if(input(LeftButton))
+      {
+         while (input(LeftButton));
+         
+         for(int i = 0; i < Exponent; i++, aux *= 10);
+         (Number >= aux) ? (Number -= aux, PrintBool = 1) : 0;
       }
       else if(input(RightButton))
       {
-         while(input(RightButton));
-         OpenMenu->ActualOption++;   BoolLCD = 1;
+         while (input(RightButton));
+         
+         for(int i = 0; i < Exponent; i++, aux *= 10);
+         ((Number + aux) <= MaxNumber) ? (Number += aux, PrintBool = 1) : 0; 
       }
-      
-      // Corregir desborde de Opcion de menu
-      OpenMenu->ActualOption < 0 ? (OpenMenu->ActualOption = OpenMenu->OptionMaxIndex) : (OpenMenu->ActualOption > OpenMenu->OptionMaxIndex ? (OpenMenu->ActualOption = 0) : 0);
-      
-      if(BoolLCD) // Evita que se actualize el LCD constantemente
-      {
-         printf(lcd_putc,"\f%s", OpenMenu->MenuOptions[OpenMenu->ActualOption]);
-         BoolLCD = 0;
-      }
-   }
+   } 
+
+   return Number;
 }
 
-void MotorSteps(int16 Amount, struct stepmotor* Motor1, int1 turnDirection) // Movimiento de un solo motor
+void MotorSteps(int16 Amount, struct step_motor* Motor1, int1 turnDirection) // Movimiento de un solo motor
 {   
    for(int16 i = 0; i < Amount; i++)
    {
@@ -130,7 +139,7 @@ void MotorSteps(int16 Amount, struct stepmotor* Motor1, int1 turnDirection) // M
    }
 }
 
-void MotorSteps(int16 Amount, struct stepmotor* Motor1, struct stepmotor* Motor2, int1 turnDirection) // Movimiento de 2 motores a la vez
+void MotorSteps(int16 Amount, struct step_motor* Motor1, struct step_motor* Motor2, int1 turnDirection) // Movimiento de 2 motores a la vez
 {
    for(int16 i = 0; i < Amount; i++)
    {
